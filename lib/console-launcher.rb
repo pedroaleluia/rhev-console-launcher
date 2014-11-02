11162e86bcf612059f63db1317ed16f00a98d550
pedroaleluia <pedro.aleluia@tad.pt>
2014-11-02 00:40:59 +0000

    Update console-launcher.rb

--- lib/console-launcher.rb
+++ lib/console-launcher.rb
@@ -29,6 +29,12 @@ require 'rbconfig'
 
 # This class provides utility methods to encapsulates RESTful access to the RHEV Manager.
 # Since the returned response is in XML this class transforms the XML Response into Ruby Objects.
+class String
+	def to_bool
+	return true if self == true || self =~ (/(true|t|yes|y|1)$/i)
+	return false if self == false || self.blank? || self =~ (/(false|f|no|n|0)$/i)
+	raise ArgumentError.new("invalid value for Boolean: \"#{self}\"")
+end
 
 class RhevManager
 
@@ -130,6 +136,7 @@ class RhevManager
       @vv.puts("toggle-fullscreen=shift+f11")
       @vv.puts("release-cursor=shift+f12")
       @vv.puts("proxy=#{vm.proxy}")
+      @vv.puts("smartcard_enabled=#{vm.smartcard_enabled}")
     ensure
       @vv.close()
     end
